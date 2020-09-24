06e9fe27419a33ca0550054997d1909bc0857df4
Aledosim <aledosim@yahoo.com.br>
2020-09-24 15:54:34 -0300

    Added GPL3 disclaimer

--- snippets.py
+++ snippets.py
@@ -5,12 +5,29 @@ Userscript for qutebrowser which saves some text that can be retrieved with the 
 The texts are saved on a json in config dir by default
 """
 
+#     Copyright Alexandre Costa do Sim (aledosim) <aledosim@yahoo.com.br>
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import logging
 log = logging.getLogger(__name__)
 log.setLevel('DEBUG')
 
-### Uncomment this line for debug log
+# --------------------------------------------------------
+# Uncomment this line for debug log
 #log.addHandler(logging.FileHandler('qute-snippets.log'))
+# --------------------------------------------------------
 
 from datetime import datetime
 log.debug('\nStarting execution in {}'.format(datetime.now()))
